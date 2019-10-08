a25c5feb21b4bc221649362a77ebbfcbb642a2ea
lionel <puchy.michal@gmail.com>
2019-10-08 10:33:14 +0200

    Step3

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -11,29 +11,36 @@ const Filter = ({value, handler}) => {
   )
 }
 
+const Forecast = ({location}) => {
+  const [forecast, setForecast] = useState()
+  const accessKey = 'a0a4841722ea35215f866af73f2c7eaf'
+
+  useEffect(() => {
+    axios
+      .get(`http://api.weatherstack.com/current?access_key=${accessKey}&query=${location}`)
+      .then( response =>  {
+        setForecast(response.data)
+      })
+  }, [])
+
+  if(forecast) {
+    return (
+      <>
+        <h1>Weather in {location}</h1>
+        <p><b>temperature:</b> {forecast.current.temperature} Celsius</p>
+        <img alt={`temperature pictogram ${forecast.current.weather_descriptions}`} src={forecast.current.weather_icons[0]} />
+        <p><b>wind:</b> {forecast.current.wind_speed} direction: {forecast.current.wind_dir}</p>
+      </>
+    )
+  } else {
+    return <p>Forecast is loading!</p>
+  }
+}
+
 const Countries = ({countries, handler}) => {
 
   if(countries.length === 1) {
-    // const countryObj = {
-    //   name: countries[0].name,
-    //   capital: countries[0].capital,
-    //   population: countries[0].population,
-    //   languages: countries[0].languages
-    // }
-
-    // const getKeys = (obj = Object.keys(countryObj)) => {
-    //   debugger
-    //     return obj.map((key, i) => {
-    //       debugger
-    //       if(Array.isArray(countryObj[key])) {
-    //         getKeys(countryObj[key])
-    //       }
-    //       return <p key={i}>{countryObj[key]}</p>
-    //     }
-    //   )
-    // }
-
-    return (//Object.keys(countryObj).map((key, i) => <p key={i}>{countryObj[key]}</p>)
+    return (
       countries.map(country => {
         return (
           <div key={country.numericCode}>
@@ -45,12 +52,7 @@ const Countries = ({countries, handler}) => {
               {country.languages.map(lang => <li key={lang.name}>{lang.name}</li>)}
             </ul>
             <img alt={`flag of ${country.name}`} src={country.flag} width="200"/>
-            {/* {getKeys()} */}
-            {/* {Object.keys(countryObj).map(
-              (key, i) => {
-                return <p key={i}>{countryObj[key]}</p>
-              }
-            )} */}
+            <Forecast location={country.capital} />
           </div>
         )
       })
@@ -91,7 +93,7 @@ function App() {
     <>
       <Filter value={newFilter} handler={handleFilter} />
       <h3>Countries</h3>
-      <Countries countries={filteredCountries}  handler={handleFilter}/>  
+      <Countries countries={filteredCountries}  handler={handleFilter}/>
     </>
   );
 }
