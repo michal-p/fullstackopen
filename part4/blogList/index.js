9c8181e1be744a999acb044134b0d89af7aae6ef
Michal Púchy <puchy.michal@gmail.com>
2020-01-08 16:51:19 +0100

    4.1 Blog list, step1

---
+++ part4/blogList/index.js
@@ -0,0 +1,49 @@
+// const app = require('./app')
+// const config = require('./utils/config')
+//
+// app.listen(config.PORT, () => console.log(`Server is running on port ${config.PORT}`))
+
+const http = require('http')
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+const blogSchema = mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number
+})
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+const mongoUrl = 'mongodb+srv://michal-p:Rotten1+@fullstackphonebook-ohxuc.mongodb.net/note-app?retryWrites=true&w=majority'
+mongoose.connect(mongoUrl, { useNewUrlParser: true })
+
+app.use(cors())
+app.use(bodyParser.json())
+
+app.get('/api/blogs', (request, response) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
+    })
+})
+
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
+    })
+})
+
+const PORT = 3003
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
---
+++ part4/blogList/utils/config.js
@@ -0,0 +1,7 @@
+let PORT = process.env.PORT
+let MONGODB_URI = process.env.MONGODB_URI
+
+module.exports = {
+  MONGODB_URI,
+  PORT
+}
