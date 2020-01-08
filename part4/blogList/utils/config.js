c29ef21dccf208ec9110ba8cad9114f4d07f77eb
Michal Púchy <puchy.michal@gmail.com>
2020-01-08 17:28:41 +0100

    Config and refactor index.js with app.js

--- part4/blogList/utils/config.js
+++ part4/blogList/utils/config.js
@@ -1,5 +1,7 @@
-let PORT = process.env.PORT
-let MONGODB_URI = process.env.MONGODB_URI
+require('dotenv').config()
+
+const PORT = process.env.PORT
+const MONGODB_URI = process.env.MONGODB_URI
 
 module.exports = {
   MONGODB_URI,
