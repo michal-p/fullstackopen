29c94d7eae1d832b4a63b1a780ed8b2602387c8e
Michal Púchy <puchy.michal@gmail.com>
2020-01-09 13:43:25 +0100

    4.5 step3 - the favouritest blog implemented

--- part4/blogList/tests/totalLikes.test.js
+++ part4/blogList/tests/totalLikes.test.js
@@ -1,24 +1,49 @@
 const listHelper = require('../utils/list_helper')
 
+const listOfBlogs = [
+  {
+    _id: '5a422aa71b54a676234d17f8',
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5,
+    __v: 0
+  },
+  {
+    _id: '5e1710181c9d440000a139df',
+    title: 'Fufu',
+    author: 'Ferdinant',
+    url: 'www.fufu.sk',
+    likes: 99,
+    __v: 0
+  },
+  {
+    _id: "5a422a851b54a676234d17f7",
+    title: "React patterns",
+    author: "Michael Chan",
+    url: "https://reactpatterns.com/",
+    likes: 7,
+    __v: 0
+  },
+  {
+    _id: "5a422aa71b54a676234d17f8",
+    title: "Go To Statement Considered Harmful",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+    likes: 5,
+    __v: 0
+  },
+  {
+    _id: "5a422b3a1b54a676234d17f9",
+    title: "Canonical string reduction",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+    likes: 12,
+    __v: 0
+  }
+]
+
 describe('total likes', () => {
-  const listOfBlogs = [
-    {
-      _id: '5a422aa71b54a676234d17f8',
-      title: 'Go To Statement Considered Harmful',
-      author: 'Edsger W. Dijkstra',
-      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-      likes: 5,
-      __v: 0
-    },
-    {
-      _id: '5e1710181c9d440000a139df',
-      title: 'Fufu',
-      author: 'Ferdinant',
-      url: 'www.fufu.sk',
-      likes: 99,
-      __v: 0
-    }
-  ]
 
   test('of empty list is zero', () => {
     const result = listHelper.totalLikes([])
@@ -32,9 +57,40 @@ describe('total likes', () => {
 
   test('of a bigger list is calculated right', () => {
     const result = listHelper.totalLikes(listOfBlogs)
-    expect(result).toBe(104)
+    expect(result).toBe(128)
   })
 })
 
+describe('the favouritest blog', () => {
+  const fav = {
+    title: 'Fufu',
+    author: 'Ferdinant',
+    likes: 99
+  }
+  test('is right', () => {
+    const result = listHelper.favouriteBlog(listOfBlogs)
+    expect(result).toEqual(fav)
+  })
+
+  const initObj = {
+    title: 'initTitle',
+    author: 'initAuthor',
+    likes: -1
+  }
+  test('of empty list is initObj', () => {
+    const result = listHelper.favouriteBlog([])
+    expect(result).toEqual(initObj)
+  })
+
+  const first = {
+    title: listOfBlogs[0].title,
+    author: listOfBlogs[0].author,
+    likes: listOfBlogs[0].likes
+  }
+  test('when list has only one blog the favourite one is that', () => {
+    const result = listHelper.favouriteBlog([listOfBlogs[0]])
+    expect(result).toEqual(first)
+  })
+})
 // Tutorial how to run only one test
 // https://jestjs.io/docs/en/api.html#testonlyname-fn-timeout
--- part4/blogList/utils/list_helper.js
+++ part4/blogList/utils/list_helper.js
@@ -2,14 +2,31 @@ const dummy = (blogs) => {
   return 1
 }
 
- const totalLikes = (blogPosts) => {
-   const likes = blogPosts.reduce((prev, cur) => {
-     return prev + cur.likes
-   }, 0)
-   return likes
- }
+const totalLikes = (blogPosts) => {
+  const likes = blogPosts.reduce((prev, curr) => {
+    return prev + curr.likes
+  }, 0)
+  return likes
+}
+
+const favouriteBlog = (blogPosts) => {
+  const initObj = {
+    title: 'initTitle',
+    author: 'initAuthor',
+    likes: -1
+  }
+  return blogPosts.reduce((acc, curr) => {
+    if(curr.likes > acc.likes) {
+      acc.title = curr.title,
+      acc.author = curr.author,
+      acc.likes = curr.likes
+    }
+    return acc
+  }, initObj)
+}
 
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favouriteBlog
 }
