d2ad7229631e035f1b795f033ca230e52247a710
Michal Púchy <puchy.michal@gmail.com>
2020-01-13 17:29:04 +0100

    Blog list small improvements

--- part4/blogList/index.js
+++ part4/blogList/index.js
@@ -1,4 +1,9 @@
 const app = require('./app')
+const http  = require('http')
 const config = require('./utils/config')
 
-app.listen(config.PORT, () => console.log(`Server is running on port ${config.PORT}`))
+const server = http.createServer(app)
+
+server.listen(config.PORT, () => {
+  console.log(`Server is running on port ${config.PORT}`)
+})
